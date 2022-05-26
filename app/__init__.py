01d40e4265af81de894d50efb17603d4e647a73f
AnnabelZhang1 <annabel.zhang1@gmail.com>
2022-05-25 20:56:13 -0400

    myfoistapp up

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,19 @@
 # SoftDev
 # P04: Forged By Land
 # 2022-05-24
+
+from flask import Flask, request, render_template, redirect, session, jsonify
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def welcome():
+	return redirect("/home")
+
+@app.route("/home", methods=['GET', 'POST'])
+def login():
+	return render_template("home.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+    app.run(debug=True)
