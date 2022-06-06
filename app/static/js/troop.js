0428829757e8d15903db83dcc066369198337f77
Alif-Abdullah0 <alifabdullah862@gmail.com>
2022-06-06 09:16:19 -0400

    pagne

--- app/static/js/troop.js
+++ app/static/js/troop.js
@@ -1,3 +1,55 @@
+class Battalion {
+  // no treason
+  // no resurrection
+  // no crippling moving speed
+  constructor(health, attack, cost, moveSpeed, ownerCol, troopCol) {
+    this.hp = health;
+    this.atk = attack;
+
+    // this value will be reduced from the emperor(player character)'s gold funds.
+    this.cost = cost;
+    this.mvSpd = moveSpeed;
+    this.ownCol = ownerCol;
+    this.trpCol = troopCol;
+  }
+
+  takeDmg(dmg) {
+    this.hp -= dmg;
+  }
+
+  heal(hp) {
+    this.hp += hp;
+  }
+
+  dealDmg(troop)  {
+    troop.takeDmg(this.atk);
+  }
+
+  payUp() {
+    return this.cost;
+  }
+
+  move(xInd, yInd, r, hex) {  //draws hexagons
+      ctx.strokeStyle = "black";
+      ctx.fillStyle = hex.color + "";
+      ctx.beginPath();
+      for (let i = 0; i < 6; i++) {
+          ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i));
+      }
+      ctx.closePath();
+      ctx.stroke();
+      ctx.fill();
+      /*
+      ctx.fillStyle = "orange";
+      ctx.textAlign = "center";
+      ctx.font = "25px Arial";
+      */
+      ctx.fillText(hex.troops + "", x, y + 10);
+  }
+
+}
+
+
 // movement of troops + combat stuff prolly
 
 let plannedActions = [[], [], [], []];
