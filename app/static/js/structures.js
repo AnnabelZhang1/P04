953aff87f8fa59db6913c82ca220fa7f2ee9092d
Alif-Abdullah0 <alifabdullah862@gmail.com>
2022-06-06 09:09:38 -0400

    troops.js + troopCanvas

--- app/static/js/structures.js
+++ app/static/js/structures.js
@@ -73,26 +73,3 @@ class Emperor {
     this.forts = []; //array of hexs w/ fort
   }
 }
-
-class Troop {
-  constructor(health, attack, cost) {
-    this.hp = health;
-    this.atk = attack;
-
-    // this value will be reduced from the emperor(player character)'s gold funds.
-    this.cost = cost;
-  }
-
-  takeDmg(dmg) {
-    this.hp -= dmg;
-  }
-
-  heal(hp) {
-    this.hp += hp;
-  }
-
-  dealDmg(troop)  {
-    troop.takeDmg(this.atk);
-  }
-
-}
